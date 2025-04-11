107de7d09d79c3a432dc0296bf7ab35a9765f5ca
nicholashodge <nicholas.m.hodge@gmail.com>
2025-04-11 11:35:46 -0500

    tests run successfully

--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 //create a "products" variable here to include at least five Product instances. Give them appropriate ProductTypeIds.
+using System.Text;
+
 List<ProductType> productTypes = new List<ProductType>(){
     new ProductType(){
         Title = "Brass Instrument",
@@ -100,23 +102,59 @@ void DisplayAllProducts(List<Product> products, List<ProductType> productTypes)
     foreach(Product product in products){
         ProductType pt = productTypes.FirstOrDefault(productTyping => productTyping.Id == product.ProductTypeId);
         Console.WriteLine($"{count}. {product.Name} - ${product.Price} ({pt.Title})");
+        count++;
     }
 
 }
 
 void DeleteProduct(List<Product> products, List<ProductType> productTypes)
 {
-    throw new NotImplementedException();
+    DisplayAllProducts(products, productTypes);
+    Console.WriteLine("Enter the number of the product to delete");
+    int chosenProduct = int.Parse(Console.ReadLine()) - 1;
+    products.RemoveAt(chosenProduct);
 }
 
 void AddProduct(List<Product> products, List<ProductType> productTypes)
 {
-    throw new NotImplementedException();
+    Product newProduct = new Product() {
+        Name = null,
+        Price = 0.00M,
+        ProductTypeId = 0
+    };
+    Console.WriteLine("Enter Product Name:");
+    newProduct.Name = Console.ReadLine();
+    Console.WriteLine("Enter Product Price:");
+    newProduct.Price = decimal.Parse(Console.ReadLine());
+    foreach(ProductType productType in productTypes){
+        Console.WriteLine($"{productType.Id}. {productType.Title}");
+    }
+    Console.WriteLine("Choose a product type");
+    newProduct.ProductTypeId = int.Parse(Console.ReadLine());
+    products.Add(newProduct);
 }
 
 void UpdateProduct(List<Product> products, List<ProductType> productTypes)
 {
-    throw new NotImplementedException();
+    //display and prompt input
+    DisplayAllProducts(products, productTypes);
+    Console.WriteLine("Enter the number of the product to edit");
+    int chosenProduct = int.Parse(Console.ReadLine()) - 1;
+    //enter name
+    Console.WriteLine("Enter a new product name (leave blank to keep current name)");
+    string newName = Console.ReadLine().Trim();
+    products[chosenProduct].Name = string.IsNullOrEmpty(newName) ? products[chosenProduct].Name : newName;
+    //enter price
+    Console.WriteLine("Enter a new product price (leave blank to keep current price)");
+    string newPrice = Console.ReadLine().Trim();
+    products[chosenProduct].Price = string.IsNullOrEmpty(newPrice) ? products[chosenProduct].Price : decimal.Parse(newPrice);
+    //select productType
+    foreach(ProductType productType in productTypes){
+        Console.WriteLine($"{productType.Id}. {productType.Title}");
+    }
+    Console.WriteLine("Choose a new product type (leave blank to keep current product type)");
+    string newProductType = Console.ReadLine().Trim();
+    products[chosenProduct].ProductTypeId = string.IsNullOrEmpty(newProductType) ? products[chosenProduct].ProductTypeId : int.Parse(newProductType);
 }
 
 // don't move or change this!
